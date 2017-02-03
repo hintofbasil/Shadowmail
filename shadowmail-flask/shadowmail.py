40e2c0b4bad660e7dab3b87cf7e8624818f10e1a
hintofbasil <hintofbasil@github.com>
2017-02-03 11:55:27 +0000

    Added SQLAlchemy support

--- shadowmail-flask/shadowmail.py
+++ shadowmail-flask/shadowmail.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 configPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config')
@@ -7,9 +8,15 @@ configPath = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config')
 app.config.from_pyfile(os.path.join(configPath, 'core.cfg'))
 app.config.from_pyfile(os.path.join(configPath, os.environ['APP_SETTINGS'] + '.cfg'))
 
+db = SQLAlchemy(app)
+
 @app.route('/')
 def hello():
     return "Hello world!"
+
+@app.cli.command()
+def initdb():
+    db.create_all()
 
 if __name__=='__main__':
     app.run()
