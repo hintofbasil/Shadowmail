cf62df6b4f87798ae48c33972f223dd8b375c97b
hintofbasil <hintofbasil@github.com>
2017-03-11 17:18:22 +0000

    Added creating of table and users to db container

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Shadowmail
+
+## Usage
+
+Generate random passwords for flask and postfix
+
+    export FLASK_DB_PASSWORD=$(pwgen -Bs1 24)
+    export POSTFIX_DB_PASSWORD=$(pwgen -Bs1 24)
+
+Lanuch docker containers
+
+    docker-compose up -d
---
+++ mysql/docker-entrypoint-initdb.d/create_shadowmail.sql
@@ -0,0 +1,9 @@
+CREATE TABLE virtual_aliases (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  alias_email varchar(100) NOT NULL,
+  real_email varchar(100) NOT NULL,
+  enabled tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (id),
+  UNIQUE KEY alias_email (alias_email)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
