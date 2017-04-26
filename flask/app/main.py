46a33d7ed11bfa5cc5b88074ece3c4f3a62748ab
hintofbasil <hintofbasil@github.com>
2017-04-26 12:37:39 +0100

    Added properly rendering of homepage html

--- flask/app/main.py
+++ flask/app/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import os
-from flask import Flask
+from flask import render_template
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
@@ -21,18 +21,9 @@ mail = Mail(app)
 from models.virtual_alias import *
 from views.api import *
 
-if app.config['DEVELOPMENT']:
-    @app.route('/')
-    def hello():
-        return """<html>
-        <head>
-            <link rel="stylesheet" type="text/css" href="static/css/homepage.css" />
-        </head>
-        <body>
-            <div id="test"></div>
-            <script src="static/js/bundle.js"></script>
-        </body>
-    </html>"""
+@app.route('/')
+def hello():
+    return render_template('homepage.html')
 
 @app.cli.command()
 def initdb():
