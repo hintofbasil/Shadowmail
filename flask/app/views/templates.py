964b500421b7faf961643d20eacf6a4383504947
hintofbasil <hintofbasil@github.com>
2017-04-28 15:05:22 +0100

    Added basic request_delete layout

--- flask/app/views/templates.py
+++ flask/app/views/templates.py
@@ -3,5 +3,12 @@ from main import app
 from flask import render_template
 
 @app.route('/')
-def hello():
+def homepage():
     return render_template('homepage.html')
+
+@app.route('/request_delete')
+def template_request_delete():
+    return render_template(
+        'click_me.html',
+        submit_value='Send request'
+    )
--- flask/app/views/test_templates.py
+++ flask/app/views/test_templates.py
@@ -5,3 +5,8 @@ def test_homepage_get():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == status.HTTP_200_OK
+
+def test_request_delete_get():
+    client = app.test_client()
+    response = client.get('/request_delete')
+    assert response.status_code == status.HTTP_200_OK
