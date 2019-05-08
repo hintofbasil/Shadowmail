81570fa17a67b7efe66f8957a75c6e4af3503636
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2019-05-08 21:47:18 +0100

    Deploy both apps to kubernetes

--- mysql/docker-entrypoint-initdb.d/create_shadowmail.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE virtual_alias (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  alias_email varchar(100) NOT NULL,
-  real_email varchar(100) NOT NULL,
-  enabled tinyint(1) NOT NULL DEFAULT 1,
-  PRIMARY KEY (id),
-  UNIQUE KEY alias_email (alias_email)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
